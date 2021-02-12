bd89c04c5fb7a8b67ccf2abeb7d465c7d8d52c6b
Jacob Olness <jacobolness@Jacobs-MacBook-Pro.local>
2021-02-12 09:04:12 -0700

    Created repositories/PiggybankRepository

---
+++ src/main/java/com/lambdaschool/piggybank/repositories/PiggybankRepository.java
@@ -0,0 +1,9 @@
+package com.lambdaschool.piggybank.repositories;
+
+import com.lambdaschool.piggybank.models.Piggybank;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PiggybankRepository extends CrudRepository<Piggybank, Long>
+{
+
+}
